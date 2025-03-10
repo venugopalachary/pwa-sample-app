708bd21f95bb17c1cffcfb4c9ac9cad813167dd9
venugopal achary <venugopalachary7396@gmail.com>
2025-03-10 11:08:38 +0530

    hi

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('pwa-cache-v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/manifest.json',
+                '/rm.png',
+                '/rm.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
